67bce96f9f3099e25916f9b2e37546c05e9edcd8
Kiennn1 <imtrungkien1904@gmail.com>
2024-03-04 01:38:51 +0700

    maybay

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool goUp, goDown, shot, gameOver;
+        int score = 0;
+        int speed = 8;
+        int UFOspeed = 10;
+
+        Random rand = new Random();
+
+        int playerSpeed = 7;
+        int index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Điểm của bạn: " + score;
+
+            if (goUp == true && player.Top > 0)
+            {
+                player.Top -= playerSpeed;
+            }
+            if (goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += playerSpeed;
+            }
+
+            ufo.Left -= UFOspeed;
+
+            if (ufo.Left + ufo.Width < 0)
+            {
+                ChangeUFO();
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "pillar")
+                {
+                    x.Left -= speed;
+
+
+                    if (x.Left < -200)
+                    {
+                        x.Left = 1000;
+                    }
+
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        GameOver();
+                    }
+
+                }
+
+                if (x is PictureBox && (string)x.Tag == "bullet")
+                {
+                    x.Left += 25;
+
+                    if (x.Left > 900)
+                    {
+                        RemoveBullet(((PictureBox)x));
+                    }
+
+
+                    if (ufo.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        RemoveBullet(((PictureBox)x));
+                        score += 1;
+                        ChangeUFO();
+                    }
+
+                }
+
+            }
+
+
+            if (player.Bounds.IntersectsWith(ufo.Bounds))
+            {
+                GameOver();
+            }
+
+            if (score > 10)
+            {
+                speed = 12;
+                UFOspeed = 15;
+            }
+
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+
+            if (e.KeyCode == Keys.Space && shot == false)
+            {
+                MakeBullet();
+                shot = true;
+            }
+            if(e.KeyCode == Keys.R && gameOver == true)
+            {
+                RestarGame();
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if(shot == true)
+            {
+                shot = false;
+            }
+
+
+        }
+        private void RestarGame()
+        {
+            goUp = false;
+            goDown = false;
+            shot = false;
+            gameOver = false;
+            score = 0;
+            speed = 8;
+            UFOspeed = 10;
+
+            txtScore.Text = "Điểm của bạn :" + score;
+
+            ChangeUFO();
+            player.Top = 132;
+
+            pillar1.Left = 602;
+            pillar2.Left = 305;
+            GameTimer.Start();
+        }
+        private void GameOver()
+        {
+            GameTimer.Stop();
+            txtScore.Text = "Điểm: " + score + "     Trò chơi kết thúc , ấn R để chơi lại !";
+            gameOver = true;
+        }
+        private void RemoveBullet(PictureBox bullet)
+        {
+            this.Controls.Remove(bullet);
+            bullet.Dispose();
+        }
+        private void MakeBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.BackColor = Color.Maroon;
+            bullet.Height = 5;
+            bullet.Width = 10;
+
+            bullet.Left = player.Left + player.Width;
+            bullet.Top = player.Top + player.Height / 2;
+
+            bullet.Tag = "bullet";
+            this.Controls.Add(bullet);
+        }
+        private void ChangeUFO()
+        {
+          
+
+            if (index > 3)
+            {
+                index = 1;
+            }
+            else 
+            {
+                index += 1;
+            }
+
+
+            switch (index) 
+            {
+                case 1:
+                    ufo.Image = Properties.Resources.alien1;
+                    break;
+                case 2:
+                    ufo.Image = Properties.Resources.alien2;
+                    break;
+                case 3:
+                    ufo.Image = Properties.Resources.alien3;
+                    break;
+            }
+            ufo.Left = 1000;
+            ufo.Top = rand.Next(20, this.ClientSize.Height - ufo.Height);
+
+        }
+    }
+}
